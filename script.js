5612023b429ae7933e1e61a7801edbb3f6e7a006
anandhi <anandhinallasivam1506@gmail.com>
2026-07-07 12:42:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// AI-Based Farmer Query Support and Advisory System
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("AI Farmer Advisory System Loaded Successfully.");
+});
